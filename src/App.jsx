98be26a5713a745b3e4f718935a5bfbd684ee73b
Simran Dhingra <simran@Simrans-MacBook-Air.local>
2022-03-30 00:50:16 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ function App() {
       <div className="container-fluid bg-dark p-4">
         <h4 className='text-center text-white'>Contest Forces</h4>
       </div>
-      <div class="d-flex justify-content-center m-2">
-        <div class="col-sm-2">
-          <button type="button" class="btn btn-outline-warning">
-            Active Users <span class="badge bg-secondary">4</span>
+      <div className="d-flex justify-content-center m-2">
+        <div className="col-sm-2">
+          <button type="button" className="btn btn-outline-warning">
+            Active Users <span className="badge bg-secondary">4</span>
           </button>
         </div>
         <div class="col-sm-2">
-          <button type="button" class="btn btn-outline-warning">
-            Active Contests <span class="badge bg-secondary">4</span>
+          <button type="button" className="btn btn-outline-warning">
+            Active Contests <span className="badge bg-secondary">4</span>
           </button>
         </div>
       </div>
---
+++ src/Test.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+
+render() {
+
+return (
+
+<div>
+
+This is Home component.
+
+</div>
+
+);
+
+}
+
+}
+
+export default Test;
